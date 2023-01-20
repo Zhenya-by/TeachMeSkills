e87bca5558706eaf5e2296a8f2d5ce11b33dc3d2
RAINKUPER\Administrator <PylotEVA02@ya.ru>
2023-01-20 12:42:29 +0300

    js домашка

---
+++ lesson13 JS/main.js
@@ -0,0 +1,133 @@
+// #### Task 1 💻
+
+// let x = 20
+// let y = 58
+// let z = 42
+// console.log (x + y + z)
+
+
+// #### Task 2 💻
+
+// let secondInMinute = 60
+// let minutePerHour = 60
+// let hoursInDay = 24
+// let daysYear = 365
+// let age = 30
+
+// let myAgeInSeconds = (age * daysYear * hoursInDay * minutePerHour *  secondInMinute) 
+
+// console.log(`Ваш возраст в секундах ${myAgeInSeconds}`)
+
+
+// #### Task 3 💻
+
+// let count = 42 // number
+// let userName = '42' // string
+
+// count = String(count) // Преоброзование переменной в строку
+// userName = Number(userName) // Преоброзование переменной в число
+
+// alert(typeof count)
+// alert(typeof userName)
+
+// let countString = '42' // Теперь переменная count строка
+// let userNameNumber = 42 // Теперь переменная count число
+
+// alert(countString + userNameNumber)
+
+// #### Task 4 💻
+
+// let a = 1
+// let b = 2
+// let c = "белых медведей"
+
+// console.log(a.toString() + b.toString() + ' ' + c)
+// console.log(`${a}${b} ${c}`);
+
+
+// #### Task 5 💻
+
+// let access = 'доступ'
+// let marine = 'морпех'
+// let ice = 'наледь'
+// let poprek = 'попрек'
+// let chopper = 'рубило'
+
+// let lengthWords = (access.length + marine.length + ice.length + poprek.length + chopper.length)
+
+// console.log(lengthWords);
+
+//-----------------вернись сюда--------------------------//
+// const one = 'строка'
+// const two = 10
+// const three = true
+
+// console.log(`Variable: one have type: ${one}`);
+//------------------------------------------------------//
+
+
+// #### Task 7 💻
+// Запросить у пользователя имя и возраст и вывести их в консоль.
+
+// let name = prompt(`Ваше имя?`);
+// let age = prompt(`Сколько вам лет?`)
+// alert(`Вас зовут ${name}, вам ${age} лет`);
+
+
+// #### Task 1 👨‍🏫 
+// В данном послании скрыт шифр! Чтобы его разгадать нам нужно соединить все вторые символы слов!
+// Создайте переменную **`cipher`** и поместите туда необходимые символы
+
+// let codeWord1 = "обернись";
+// let codeWord2 = "неужели";
+// let codeWord3 = "огурцы";
+// let codeWord4 = "липкие";
+// let codeWord5 = "?!";
+
+// const cipher = (codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1])
+// console.log(cipher);
+
+
+// #### Task 1 💻
+
+// Создайте переменные и присвойте им значения:
+
+// ```javascript
+//     'true'
+//     false
+//     17
+//     undefined
+//     null
+// ```
+// > Затем с помощью оператора **`typeof`** выведите в консоль все типы созданных значений переменных.
+
+// const man = 'true'
+// const dog = true
+// const age = 17
+// const city = undefined
+// const car = null
+
+// console.log(typeof 'true');
+// console.log(typeof true);
+// console.log(typeof 17);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+// #### Task 2 💻
+// Даны две переменные:
+// > С помощью условий выведите в консоль разработчика наибольшее число.
+
+// let height = 15
+// let width = 20
+
+// console.log(Math.max(15, 20));
+
+
+// #### Task 3 💻
+// Написать перебор от 1 до 20, где выведутся все числа кратные трём.
+// > Для вычисления кратности обратите внимание на оператор %
+
+
+
+
