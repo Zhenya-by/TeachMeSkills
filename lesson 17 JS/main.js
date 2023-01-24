0e40b5bc0436ff553396091a41756e3113b8fa93
RAINKUPER\Administrator <PylotEVA02@ya.ru>
2023-01-25 01:30:01 +0300

    готовое дз

--- lesson 17 JS/main.js
+++ lesson 17 JS/main.js
@@ -1,9 +1,9 @@
-let people =[ 
-    {name: 'Alex', bill: 3000},
-    {name: 'Ivan', bill: 2000},
-    {name: 'Vadim', bill: 1000},
-    {name: 'Vova', bill: 300}
-]
+// let people =[ 
+//     {name: 'Alex', bill: 3000},
+//     {name: 'Ivan', bill: 2000},
+//     {name: 'Vadim', bill: 1000},
+//     {name: 'Vova', bill: 300}
+// ]
 
 // Меторд map
 // const peopleSalary = people.map (item => (item.bill * 3)) ;
@@ -22,12 +22,12 @@ let people =[
 
 
 //Метод Concat
-let people2 =[ 
-    {name: 'Alex', bill: 3000},
-    {name: 'Ivan', bill: 2000},
-    {name: 'Vadim', bill: 1000},
-    {name: 'Vova', bill: 300}
-]
+// let people2 =[ 
+//     {name: 'Alex', bill: 3000},
+//     {name: 'Ivan', bill: 2000},
+//     {name: 'Vadim', bill: 1000},
+//     {name: 'Vova', bill: 300}
+// ]
 // let people3 = people.concat(people2)
 
 // console.log(people3);
@@ -42,3 +42,94 @@ let people2 =[
 //     return item.bill >= 1000; 
 // })
 // console.log(result ,result2);
+
+
+// Метод findIndex 
+// let peopleSalary = people2.findIndex(index => index.bill === 300)
+// let peopleSalary2 = people2.findIndex(index => index.name === 'Yura') // возвращает -1 так как в массиве нет объекта с таким индексом.
+
+// console.log(peopleSalary, peopleSalary2);
+
+
+// Метод Push - добавляет элементы в конец
+// people2.push({name: 'Yura', bill: 4000},
+//     {name: 'Sasha', bill: 2500},
+//     {name: 'Sasha', bill: 2500},
+//     {name: 'Dima', bill: 1500},
+//     {name: 'Valera', bill: 2200},
+//     {name: 'Kolya', bill: 1800},
+//     {name: 'Timofei', bill: 4500})
+
+// console.log(people2);
+
+
+// Метод Slice - не мутирует массив. Забыть о splice (мутирует массив, может создать проблеммы).
+// const sliceArr = people2.slice(11) 
+
+// console.log(sliceArr);
+
+
+// Метод pop - удаляет последний элемент массива
+// people2.pop()
+// people2.pop()
+// people2.pop()
+// people2.pop()
+// console.log(people2);
+
+
+// Метод unshift: добовляет элементы в начало массива 
+let people2 =[ 
+    {name: 'Alex', bill: 3000},
+    {name: 'Ivan', bill: 2000},
+    {name: 'Vadim', bill: 1000},
+    {name: 'Vova', bill: 300},
+]
+
+// people2.unshift({name: 'Sasha', bill: 2500},
+// {name: 'Valera', bill: 2200})
+
+// console.log(people2);
+
+
+// Метод shift: удаляет элементы массива
+// people2.shift(), people2.shift()
+// console.log(people2);
+
+
+// Метод some
+// const someArrays = people2.some(item => item.name === 'Yura'); // если нет указанного чел. вернет false
+
+// // console.log(isEqualArrays)
+
+// // Метод Every
+// const someArrays1 = people2.every(item => item.name != 'Yura'); 
+
+// console.log(someArrays, someArrays1)
+
+
+// Метод Sort
+let numbers =[3,25,63,51,4321,42]
+// Сортировка по возрастанию
+function startNumbersStart(a, b) {
+    if (a > b) 
+    return 1
+    if (a == b)
+    return 0
+    if (a < b)
+    return -1
+}
+numbers.sort(startNumbersStart)
+console.log(numbers);
+
+
+// сортировка в обратном порядке
+function startNumbersEnd(a, b) {
+    if (a > b) 
+    return -1
+    if (a == b)
+    return 0
+    if (a < b)
+    return 1
+}
+numbers.sort(startNumbersEnd)
+console.log(numbers);
