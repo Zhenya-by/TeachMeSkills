eb90cb974ed3e51c56d3fbb603ecb59e2356b7b3
RAINKUPER\Administrator <PylotEVA02@ya.ru>
2023-02-04 00:10:50 +0300

    fix dz

--- Todo-list/main.js
+++ Todo-list/main.js
@@ -1,11 +1,8 @@
 const root = document.getElementById('root')
 root.style.margin = '0 auto'
 root.style.marginTop = '10%'
-root.style.width = '600px'
-root.style.height = '500px'
 root.style.borderRadius = '20px'
 root.style.border = '3px solid black'
-root.style.backgroundColor = '#D5D5D5'
 
 // btn
 const deliteAll = document.createElement('button') // создаем новый элемент <button>
@@ -35,10 +32,11 @@ divWrap.classList.add('wrap')
 // div innerBig
 const divInnerBig = document.createElement('div');
 root.appendChild(divInnerBig);
+// divInnerBig.innerHTML = ''
 divInnerBig.classList.add('innerBig')
 
 // btnEnter
-const enter = document.createElement('button')
+const enter = document.createElement(`button`)
 divInnerBig.appendChild(enter)
 const textButtonEnter = document.createTextNode('v')
 enter.appendChild(textButtonEnter)
@@ -64,6 +62,43 @@ divInnerBig.appendChild(dateInput);
 dateInput.classList.add('date-input');
 dateInput.setAttribute(`type`, `text`);
 dateInput.placeholder = 'Date';
+
+
+
+
+// div innerBig
+const divInnerBig2 = document.createElement('div');
+root.appendChild(divInnerBig2);
+// divInnerBig.innerHTML = ''
+divInnerBig2.classList.add('innerBig2')
+
+// btnEnter
+const enter2 = document.createElement(`button`)
+divInnerBig2.appendChild(enter2)
+const textButtonEnter2 = document.createTextNode('v')
+enter2.appendChild(textButtonEnter2)
+enter2.classList.add('enterBnt')
+
+// input
+const todoText2 = document.createElement('input')
+divInnerBig2.appendChild(todoText2)
+todoText2.setAttribute(`type`, `text`);
+todoText2.classList.add('todoText')
+todoText2.placeholder = 'Todo text'
+
+// btnClose inner 
+const closeBtn2 = document.createElement('button')
+divInnerBig2.appendChild(closeBtn2)
+const textCloseBtn2 = document.createTextNode('X')
+closeBtn2.appendChild(textCloseBtn2)
+closeBtn2.classList.add('closeBtn')
+
+// date
+const dateInput2 = document.createElement('input');
+divInnerBig2.appendChild(dateInput2);
+dateInput2.classList.add('date-input');
+dateInput2.setAttribute(`type`, `text`);
+dateInput2.placeholder = 'Date';
 
 
 
